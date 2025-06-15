85b5c0deff913d56a18735a142428ca4a2301916
Yuuki <adg.jmp.adg.adg@gmail.com>
2025-06-15 16:24:26 +0900

    Initial commit: Flask ToDo app with PostgreSQL

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.model):
+    id = db.column(db.Integer, primary_key=True)
+    content = db.column(db.string(200), nullable=False)
+
+# トップページ（タスクリスト）
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+# タスクの追加
+app.route('/add', methods=['POST'])
+def add():
+    content = request.form['content']
+    new_task = Task(content=content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
+
+# タスクの削除
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__name__':
+    # 初回のみ以下を実行してテーブルを作成
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
