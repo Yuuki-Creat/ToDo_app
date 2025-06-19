b566221b0a50f83494c9fe02ad037123bef86d80
Yuuki <adg.jmp.adg.adg@gmail.com>
2025-06-20 07:11:44 +0900

    Login add repair secret_key default_key

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')  # 追加
+app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
