bbacfff04358d6f194aef04397cc9766a55c2a9e
Yuuki <adg.jmp.adg.adg@gmail.com>
2025-06-22 15:52:30 +0900

    inventory window repair

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'DEFAULT_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
